88b25afe8bbb2e17d63e013795b1f85299ef5d2d
Gareth Johns <info@garethjohnsdesign.com>
2018-12-17 10:48:23 +0000

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-// $(document).on('turbolinks:load', function() {
-
 // 1. Foundation
 // --------------------
 
@@ -12,51 +10,3 @@ Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-
 Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
 
 $(document).foundation();
-
-// 2. Animate on Scroll
-// --------------------
-
-$(function() {
-  AOS.init({ 
-   offset: 64,
-   easing: 'ease-in-out-quart', 
-   duration: 600
-   });
-});
-
-$(function() {
-window.addEventListener('load', AOS.refresh);
-});
-
-
-var prev = 0;
-var $window = $(window);
-var nav = $('.scrollhide-nav');
-
-$window.off('scroll');
-$window.on('scroll', function() {
-    var scrollTop = $window.scrollTop();
-    if (scrollTop < 0) scrollTop = 0;
-    nav.toggleClass('hidden', scrollTop > prev);
-    prev = scrollTop;
-});
-
-$('.video').lightGallery({
-    counter: false,
-    youtubePlayerParams: {
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0,
-        controls: 0
-    },
-    vimeoPlayerParams: {
-        autoplay: 0,
-        title : 0,
-        byline : 0,
-        portrait : 0,
-        color : 'FFFFFF'     
-    }
-});
-
-
-// });
